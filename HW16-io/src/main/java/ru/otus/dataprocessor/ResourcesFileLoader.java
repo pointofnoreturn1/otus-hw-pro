c59dc0d32a342669d80eb77eb4124b9085e919e8
Vadim Kurnyshov <vaku878@gmail.com>
2025-01-30 08:33:33 +0400

    fix hw16-io

--- HW16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ HW16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 import ru.otus.model.Measurement;
 
@@ -17,8 +18,9 @@ public class ResourcesFileLoader implements Loader {
     @Override
     public List<Measurement> load() {
         try {
-            return new ObjectMapper().readValue(new File("src/test/resources/" + fileName), new TypeReference<>() {});
-        } catch (IOException e) {
+            return new ObjectMapper()
+                    .readValue(new File(ClassLoader.getSystemResource(fileName).toURI()), new TypeReference<>() {});
+        } catch (IOException | URISyntaxException e) {
             throw new FileProcessException(e);
         }
     }
