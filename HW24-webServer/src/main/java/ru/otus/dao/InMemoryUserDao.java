c240efa24fd21cdb3ec568376b75e4555e1099a3
Vadim Kurnyshov <vaku878@gmail.com>
2025-03-25 20:38:52 +0400

    fix hw24

--- HW24-webServer/src/main/java/ru/otus/dao/InMemoryUserDao.java
+++ HW24-webServer/src/main/java/ru/otus/dao/InMemoryUserDao.java
@@ -6,15 +6,15 @@ import java.util.Optional;
 import ru.otus.model.User;
 
 public class InMemoryUserDao implements UserDao {
-    private final Map<Long, User> users;
+    private final Map<String, User> users;
 
     public InMemoryUserDao() {
         users = new HashMap<>();
-        users.put(1L, new User(1L, "admin", "admin", "11111"));
+        users.put("admin", new User(1L, "admin", "admin", "11111"));
     }
 
     @Override
     public Optional<User> findByLogin(String login) {
-        return users.values().stream().filter(v -> v.login().equals(login)).findFirst();
+        return Optional.of(users.get(login));
     }
 }
